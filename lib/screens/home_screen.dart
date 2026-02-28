810ca91f46e8998b9f0b057bbdf72d3336f6bd1a
Albert4565 <dashkinalbert91@gmail.com>
2026-02-28 16:08:59 +0300

    feat: add linear gradient in Home Screen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [Color(0xFFFB7100), Color(0xFFFF953F)],
+        ),
+      ),
+    );
+  }
+}
