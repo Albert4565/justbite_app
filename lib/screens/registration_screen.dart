69c79863f339f3bf18e12ce45fbaf1a8f21fd1e3
Albert4565 <dashkinalbert91@gmail.com>
2026-03-20 22:46:33 +0300

    chore: add

---
+++ lib/screens/registration_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class RegistrationScreen extends StatelessWidget {
+  const RegistrationScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFF5F5F5),
+      body: Center(
+        child: Text(
+          "Регистрация",
+          style: TextStyle(
+            fontFamily: "Montserrat",
+            fontWeight: FontWeight.w700,
+            fontSize: 24  ,
+          ),
+        ),
+      ),
+    );
+  }
+}
