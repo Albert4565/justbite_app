431d5b1be4eb5dd2dab642120dcfcd4a10d6ba29
Albert4565 <dashkinalbert91@gmail.com>
2026-03-26 08:30:05 +0300

    feat: removed screen flip

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:justbite_app/screens/start_screen.dart';
+import 'package:flutter/services.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
   runApp(const MyApp());
 }
 
